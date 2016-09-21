cdccf2dfe4792788fe013b66fa6b3d65efef02b3
Steve Temple <steve@gibedigital.com>
2016-09-21 22:54:58 +0100

    Add ParentAttribute

---
+++ Gibe.DittoProcessors/Processors/ParentAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gibe.DittoProcessors.Processors
+{
+	public class ParentAttribute : TestableDittoProcessorAttribute
+	{
+		public override object ProcessValue()
+		{
+			return Context.Content.Parent;
+		}
+	}
+}
