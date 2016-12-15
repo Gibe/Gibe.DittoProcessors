8ca1a5929ff55142497f2ca675c180c550768b4d
Karl Tynan <hello@karltynan.co.uk>
2016-12-15 12:22:37 +0000

    String is not empty processor

---
+++ Gibe.DittoProcessors/Processors/StringIsNotEmptyAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Gibe.DittoProcessors.Processors
+{
+	public class StringIsNotEmptyAttribute : TestableDittoProcessorAttribute
+	{
+		public override object ProcessValue()
+		{
+			if (Value == null) throw new NullReferenceException();
+
+			var stringValue = Value as string;
+
+			return !String.IsNullOrEmpty(stringValue);
+		}
+	}
+}
