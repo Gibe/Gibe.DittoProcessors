24f372619f061086ffacd7370041d85b037f586d
Zia Siddique <zia.siddique@gibedigital.com>
2017-04-10 16:25:26 +0100

    Revert "Navigate up many parents"
    This reverts commit f430cc765b43b22d56ce20dae47a331401f0dbd3.

--- Gibe.DittoProcessors/Processors/ParentAttribute.cs
+++ Gibe.DittoProcessors/Processors/ParentAttribute.cs
@@ -8,22 +8,9 @@ namespace Gibe.DittoProcessors.Processors
 {
 	public class ParentAttribute : TestableDittoProcessorAttribute
 	{
-		private readonly uint _parentDepth;
-
-		public ParentAttribute(uint parentDepth = 1)
-		{
-			_parentDepth = parentDepth;
-		}
-
 		public override object ProcessValue()
 		{
-			var content = Context.Content;
-			for (var i = 0; i < _parentDepth; i++)
-			{
-				content = content.Parent;
-			}
-
-			return content;
+			return Context.Content.Parent;
 		}
 	}
 }
