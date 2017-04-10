f430cc765b43b22d56ce20dae47a331401f0dbd3
Zia Siddique <zia.siddique@gibedigital.com>
2017-04-10 16:02:05 +0100

    Navigate up many parents

--- Gibe.DittoProcessors/Processors/ParentAttribute.cs
+++ Gibe.DittoProcessors/Processors/ParentAttribute.cs
@@ -8,9 +8,22 @@ namespace Gibe.DittoProcessors.Processors
 {
 	public class ParentAttribute : TestableDittoProcessorAttribute
 	{
+		private readonly uint _parentDepth;
+
+		public ParentAttribute(uint parentDepth = 1)
+		{
+			_parentDepth = parentDepth;
+		}
+
 		public override object ProcessValue()
 		{
-			return Context.Content.Parent;
+			var content = Context.Content;
+			for (var i = 0; i < _parentDepth; i++)
+			{
+				content = content.Parent;
+			}
+
+			return content;
 		}
 	}
 }
