02adf050ae4e5672239aec2c31cab362824a3cbc
Zia Siddique <zia.siddique@gibedigital.com>
2017-04-10 16:37:12 +0100

    Implement AncestorAttribute

---
+++ Gibe.DittoProcessors/Processors/AncestorAttribute.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Gibe.UmbracoWrappers;
+using Umbraco.Core.Models;
+
+namespace Gibe.DittoProcessors.Processors
+{
+	public class AncestorAttribute : InjectableProcessorAttribute
+	{
+		public Func<IUmbracoWrapper> UmbracoWrapper => Inject<IUmbracoWrapper>();
+
+		private readonly int _maxDepth;
+		private readonly string _docTypeAlias;
+
+		public AncestorAttribute()
+		{
+		}
+
+		public AncestorAttribute(int maxDepth)
+		{
+			_maxDepth = maxDepth;
+		}
+
+		public AncestorAttribute(string docTypeAlias)
+		{
+			_docTypeAlias = docTypeAlias;
+		}
+
+		public override object ProcessValue()
+		{
+			if (!string.IsNullOrEmpty(_docTypeAlias))
+			{
+				return UmbracoWrapper().AncestorOrSelf(Context.Content, _docTypeAlias);
+			}
+			else if (_maxDepth != null)
+			{
+				return UmbracoWrapper().AncestorOrSelf(Context.Content, _maxDepth);
+			}
+			return UmbracoWrapper().Ancestor(Context.Content);
+		}
+	}
+}
