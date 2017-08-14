780ceba8912cf56fa82e55c7fed059bc59693e9f
Zia Siddique <zia.siddique@gibedigital.com>
2017-08-14 12:31:12 +0100

    Add UmbracoProperty to GridContentMediaValue

--- Gibe.DittoProcessors/Processors/Models/GridContentMediaValue.cs
+++ Gibe.DittoProcessors/Processors/Models/GridContentMediaValue.cs
@@ -1,9 +1,14 @@
-﻿namespace Gibe.DittoProcessors.Processors.Models
+﻿using Our.Umbraco.Ditto;
+
+namespace Gibe.DittoProcessors.Processors.Models
 {
 	public class GridContentMediaValue
 	{
 		public int Id { get; set; }
+
+		[UmbracoProperty("umbracoFile")]
 		public string Image { get; set; }
+
 		public string Caption { get; set; }
 	}
 }
