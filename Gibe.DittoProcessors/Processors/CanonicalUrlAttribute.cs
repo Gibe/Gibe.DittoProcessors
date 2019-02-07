3b9e8b3bb4b3ac2aab91c43b06856340ebe7e00e
Steve Temple <steve@gibedigital.com>
2019-02-07 10:25:33 +0000

    Update CanonicalUrlAttribute.cs

--- Gibe.DittoProcessors/Processors/CanonicalUrlAttribute.cs
+++ Gibe.DittoProcessors/Processors/CanonicalUrlAttribute.cs
@@ -5,7 +5,14 @@ namespace Gibe.DittoProcessors.Processors
 {
 	public class CanonicalUrlAttribute : TestableDittoProcessorAttribute
 	{
-		public override object ProcessValue() => Context.Content.DocumentTypeAlias.InvariantEquals("home")
+		private string _homeDocTypeAlias;
+	
+		public CanonicalUrlAttribute(string homeDocTypeAlias = "home") 
+		{
+			_homeDocTypeAlias = homeDocTypeAlias;
+		}
+		
+		public override object ProcessValue() => Context.Content.DocumentTypeAlias.InvariantEquals(_homeDocTypeAlias)
 			? Context.Content.UrlAbsolute().Replace(Context.Content.Url, "/")
 			: Context.Content.UrlAbsolute();
 	}
